df10503dfb0d1ae71a65ff9a265f7da235dccc9a
cakraadhana <115514199+cakraadhana@users.noreply.github.com>
2024-01-15 08:32:19 +0700

    Nilai

---
+++ nilai/src/main/java/com/cakra/nilai/repository/NilaiRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.cakra.nilai.repository;
+
+import com.cakra.nilai.entity.Nilai;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ *
+ * @author brily
+public interface NilaiRepository extends JpaRepository<Nilai, Long>{
+    
+}
