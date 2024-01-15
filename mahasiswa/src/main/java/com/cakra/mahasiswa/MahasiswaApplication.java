27b8856f1fbd6ab7fa4a4e00d872c5b00c3c1271
cakraadhana <115514199+cakraadhana@users.noreply.github.com>
2024-01-15 08:34:05 +0700

    mahasiswa

---
+++ mahasiswa/src/main/java/com/cakra/mahasiswa/MahasiswaApplication.java
@@ -0,0 +1,13 @@
+package com.cakra.mahasiswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahasiswaApplication.class, args);
+	}
+
+}
